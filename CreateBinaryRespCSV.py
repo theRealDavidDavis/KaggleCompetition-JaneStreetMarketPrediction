4efe99e591847bd6fa46a21f5cb1c4a4885f8a89
David Davis <54605208+theRealDavidDavis@users.noreply.github.com>
2021-01-02 07:44:16 -0600

    Add files via upload
    This script was made to classify negative response values as zero and positive values as 1 within the training data set for the Jane Street Market Prediction Competition. 
    
    The purpose of this is to create a csv file that can be analyzed within Minitab as Minitab makes EDA a very easy process.

---
+++ CreateBinaryRespCSV.py
@@ -0,0 +1,21 @@
+import time
+import pandas as pd
+import numpy as np
+import datatable as dtable
+
+start = time.time()
+print('Start Time = ', time.time() - start)
+
+data = dtable.fread("[file path]/train.csv").to_pandas()
+data = data.drop(['date', 'ts_id'], axis=1)
+data = data.astype({c: np.float32 for c in data.select_dtypes(include='float64').columns})
+data = data.query('weight > 0')
+data = data.fillna(0)
+
+y = data[['resp']]
+y = [0 if i < 0 else 1 for i in y['resp']]
+binary_df = pd.DataFrame(data=y)
+binary_df.to_csv('BinaryResp2.csv')
+
+print('Length = ', len(y))
+print('Done = ', time.time() - start)
